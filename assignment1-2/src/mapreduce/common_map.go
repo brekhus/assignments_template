c83046f56adf256b3a4c73c2295c57501b5efa23
Nick Brekhus <nick.brekhus@gmail.com>
2019-08-09 19:34:12 -0700

    Implement Serial Map/Reduce

--- assignment1-2/src/mapreduce/common_map.go
+++ assignment1-2/src/mapreduce/common_map.go
@@ -1,7 +1,11 @@
 package mapreduce
 
 import (
+	"encoding/json"
+	"fmt"
 	"hash/fnv"
+	"io/ioutil"
+	"os"
 )
 
 // doMap does the job of a map worker: it reads one of the input files
@@ -14,33 +18,33 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the filename for this map task's input to reduce task number
-	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
-	// below doMap) should be used to decide which file a given key belongs into.
-	//
-	// The intermediate output of a map task is stored in the file
-	// system as multiple files whose name indicates which map task produced
-	// them, as well as which reduce task they are for. Coming up with a
-	// scheme for how to store the key/value pairs on disk can be tricky,
-	// especially when taking into account that both keys and values could
-	// contain newlines, quotes, and any other character you can think of.
-	//
-	// One format often used for serializing data to a byte stream that the
-	// other end can correctly reconstruct is JSON. You are not required to
-	// use JSON, but as the output of the reduce tasks *must* be JSON,
-	// familiarizing yourself with it here may prove useful. You can write
-	// out a data structure as a JSON string to a file using the commented
-	// code below. The corresponding decoding functions can be found in
-	// common_reduce.go.
-	//
-	//   enc := json.NewEncoder(file)
-	//   for _, kv := ... {
-	//     err := enc.Encode(&kv)
-	//
-	// Remember to close the file after you have written all the values!
-	// Use checkError to handle errors.
+	inF, err := os.Open(inFile)
+	checkError(err)
+	b, err := ioutil.ReadAll(inF)
+	checkError(err)
+	var writers []*os.File
+	for i := 0; i < nReduce; i++ {
+		outF, err := os.Create(reduceName(jobName, mapTaskNumber, i))
+		fmt.Printf("create: %s\n", reduceName(jobName, mapTaskNumber, i))
+		checkError(err)
+		writers = append(writers, outF)
+	}
+	defer func() {
+		for _, writer := range writers {
+			writer.Close()
+		}
+	}()
+	var encoders []*json.Encoder
+	for _, writer := range writers {
+		encoder := json.NewEncoder(writer)
+		encoders = append(encoders, encoder)
+	}
+
+	for _, kv := range mapF(inFile, string(b)) {
+		slot := ihash(kv.Key) % uint32(nReduce)
+		encoder := encoders[slot]
+		encoder.Encode(kv)
+	}
 }
 
 func ihash(s string) uint32 {
--- assignment1-2/src/mapreduce/common_reduce.go
+++ assignment1-2/src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -10,27 +16,31 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the intermediate file for this reduce task from map task number
-	// m using reduceName(jobName, m, reduceTaskNumber).
-	// Remember that you've encoded the values in the intermediate files, so you
-	// will need to decode them. If you chose to use JSON, you can read out
-	// multiple decoded values by creating a decoder, and then repeatedly calling
-	// .Decode() on it until Decode() returns an error.
-	//
-	// You should write the reduced output in as JSON encoded KeyValue
-	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
-	// you to use JSON here because that is what the merger than combines the
-	// output from all the reduce tasks expects. There is nothing "special" about
-	// JSON -- it is just the marshalling format we chose to use. It will look
-	// something like this:
-	//
-	// enc := json.NewEncoder(mergeFile)
-	// for key in ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Use checkError to handle errors.
+	inF, err := os.Open(reduceName(jobName, nMap, reduceTaskNumber))
+	checkError(err)
+	defer inF.Close()
+
+	outF, err := os.Create(mergeName(jobName, reduceTaskNumber))
+	checkError(err)
+	defer outF.Close()
+
+	decoder := json.NewDecoder(inF)
+	keys := make(map[string][]string)
+	for {
+		var kv KeyValue
+		err := decoder.Decode(&kv)
+		if err != nil {
+			if err != io.EOF {
+				checkError(err)
+			}
+			break
+		}
+		keys[kv.Key] = append(keys[kv.Key], kv.Value)
+	}
+
+	encoder := json.NewEncoder(outF)
+	for k, v := range keys {
+		value := reduceF(k, v)
+		encoder.Encode(KeyValue{Key: k, Value: value})
+	}
 }
