aa4c6db040d24226b203e25aa1f1827923c160ba
Nick Brekhus <nick.brekhus@gmail.com>
2019-08-09 21:53:27 -0700

    Copy project.

---
+++ assignment1-3/src/mapreduce/common_map.go
@@ -0,0 +1,52 @@
+package mapreduce
+
+import (
+	"encoding/json"
+	"hash/fnv"
+	"io/ioutil"
+	"os"
+)
+
+// doMap does the job of a map worker: it reads one of the input files
+// (inFile), calls the user-defined map function (mapF) for that file's
+// contents, and partitions the output into nReduce intermediate files.
+func doMap(
+	jobName string, // the name of the MapReduce job
+	mapTaskNumber int, // which map task this is
+	inFile string,
+	nReduce int, // the number of reduce task that will be run ("R" in the paper)
+	mapF func(file string, contents string) []KeyValue,
+) {
+	inF, err := os.Open(inFile)
+	checkError(err)
+	b, err := ioutil.ReadAll(inF)
+	checkError(err)
+	var writers []*os.File
+	for i := 0; i < nReduce; i++ {
+		outF, err := os.Create(reduceName(jobName, mapTaskNumber, i))
+		checkError(err)
+		writers = append(writers, outF)
+	}
+	defer func() {
+		for _, writer := range writers {
+			writer.Close()
+		}
+	}()
+	var encoders []*json.Encoder
+	for _, writer := range writers {
+		encoder := json.NewEncoder(writer)
+		encoders = append(encoders, encoder)
+	}
+
+	for _, kv := range mapF(inFile, string(b)) {
+		slot := ihash(kv.Key) % uint32(nReduce)
+		encoder := encoders[slot]
+		encoder.Encode(kv)
+	}
+}
+
+func ihash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
---
+++ assignment1-3/src/mapreduce/common_reduce.go
@@ -0,0 +1,48 @@
+package mapreduce
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+// doReduce does the job of a reduce worker: it reads the intermediate
+// key/value pairs (produced by the map phase) for this task, sorts the
+// intermediate key/value pairs by key, calls the user-defined reduce function
+// (reduceF) for each key, and writes the output to disk.
+func doReduce(
+	jobName string, // the name of the whole MapReduce job
+	reduceTaskNumber int, // which reduce task this is
+	nMap int, // the number of map tasks that were run ("M" in the paper)
+	reduceF func(key string, values []string) string,
+) {
+	outF, err := os.Create(mergeName(jobName, reduceTaskNumber))
+	checkError(err)
+	defer outF.Close()
+
+	keys := make(map[string][]string)
+	for i := 0; i < nMap; i++ {
+		inF, err := os.Open(reduceName(jobName, i, reduceTaskNumber))
+		checkError(err)
+		defer inF.Close()
+
+		decoder := json.NewDecoder(inF)
+		for {
+			var kv KeyValue
+			err := decoder.Decode(&kv)
+			if err != nil {
+				if err != io.EOF {
+					checkError(err)
+				}
+				break
+			}
+			keys[kv.Key] = append(keys[kv.Key], kv.Value)
+		}
+	}
+
+	encoder := json.NewEncoder(outF)
+	for k, v := range keys {
+		value := reduceF(k, v)
+		encoder.Encode(KeyValue{Key: k, Value: value})
+	}
+}
